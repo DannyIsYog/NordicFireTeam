03c96f05256107d7689a8d78b02afb2f62edd2c4
André Antunes <andre.r.antunes@ist.utl.pt>
2021-05-08 02:37:47 +0100

    Create Music Animator, Refactor PubManager

--- Assets/Scripts/Groovy.cs
+++ Assets/Scripts/Groovy.cs
@@ -7,60 +7,39 @@ public class Groovy : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] public float RandomMax;
-    [SerializeField] public float RandomMin;
     [SerializeField] public SpriteRenderer Image;
     [SerializeField] public AudioClip[] _clipsTV;
     [SerializeField] public AudioSource _audioTV;
     [SerializeField] public AudioSource _audioMusic;
-    private float RandomNum;
-    public bool TV;
-
-    public event Action TVOn;
-    public event Action TVOff;
+    [SerializeField] public Animator _animatorMusic;
+    public PubManager Pubs;
 
     void Start()
     {
-        TV = true;
-       RandomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
         _audioTV.clip = _clipsTV[0];
         _audioTV.Play();
-        _audioMusic.mute = true;
+
+        Pubs.TVOn += TurnTVOn;
+        Pubs.TVOff += TurnTVOff;
     }
 
     void Update()
     {
-        if(Time.unscaledTime >= RandomNum)
-        {
-            SwapMusicState();
-        }
+
     }
 
-    void SwapMusicState()
+    void TurnTVOn()
     {
-        if (TV)
-        {
-            TVOn?.Invoke();
-            TV = false;
+        _animatorMusic.SetTrigger("Switch");
+        _audioTV.clip = _clipsTV[0];
+        _audioTV.Play();
+    }
 
-            RandomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
-            RandomNum += Time.unscaledTime;
-            _audioMusic.mute = false;
-            _audioTV.clip = _clipsTV[1];
-            _audioTV.Play();
-
-        }
-        else
-        {
-            TVOff?.Invoke();
-            TV = true;
-
-            RandomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
-            RandomNum += Time.unscaledTime;
-            _audioMusic.mute = true;
-            _audioTV.clip = _clipsTV[0];
-            _audioTV.Play();
-        }
+    void TurnTVOff()
+    {
+        _animatorMusic.SetTrigger("Switch");
+        _audioTV.clip = _clipsTV[1];
+        _audioTV.Play();
     }
 
 
--- Assets/Scripts/PubManager.cs
+++ Assets/Scripts/PubManager.cs
@@ -1,31 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PubManager : MonoBehaviour
 {
-
+    [SerializeField] public float RandomMax;
+    [SerializeField] public float RandomMin;
     [SerializeField] public GameObject[] Customers;
     public int[] places;
-    public Groovy grooves;
     private int _total;
+    public bool TV;
+    private float _randomNum;
+
+    #region Events
+    public event Action TVOn;
+    public event Action TVOff;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        TV = true;
+        _randomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
+
         _total = Customers.Length;
         for(int i = 0; i < _total; i++)
         {
             places[i] = i;
         }
         SortCustomers();
-        grooves.TVOn += SortCustomers;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.unscaledTime >= _randomNum)
+        {
+            SwapMusicState();
+        }
     }
 
     private void SortCustomers()
@@ -33,10 +46,31 @@ public class PubManager : MonoBehaviour
         for (int i = 0; i < _total; i++)
         {
             int temp = places[i];
-            int randomIndex = Random.Range(i, _total);
+            int randomIndex = UnityEngine.Random.Range(i, _total);
             places[i] = places[randomIndex];
             places[randomIndex] = temp;
             Debug.Log(places[i]);
+        }
+    }
+
+    void SwapMusicState()
+    {
+        if (TV)
+        {
+            TVOff?.Invoke();
+            TV = false;
+            _randomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
+            _randomNum += Time.unscaledTime;
+
+        }
+        else
+        {
+            TVOn?.Invoke();
+            TV = true;
+
+            SortCustomers();
+            _randomNum = UnityEngine.Random.Range(RandomMin, RandomMax);
+            _randomNum += Time.unscaledTime;
         }
     }
 }
