6aa5fe06097545c0814e651586a2f18912f48d00
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2021-05-08 12:03:46 +0100

    Added Sprite and Walking Animations

--- Assets/Scenes/Gameplay/Scripts/Throwable.cs
+++ Assets/Scenes/Gameplay/Scripts/Throwable.cs
@@ -22,8 +22,8 @@ public class Throwable : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnTriggerEnter2D(Collider2D collision) { 
+    
         if(collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerController>().GotHit();
